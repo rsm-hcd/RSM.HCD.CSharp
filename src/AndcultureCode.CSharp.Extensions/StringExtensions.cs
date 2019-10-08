3e9ba2b89d182b958592781059159c2ebe110715
Winton DeShong <contact@winton.me>
2019-10-08 08:37:54 -0400

    Removed unnecessary code for issue #7

--- src/AndcultureCode.CSharp.Extensions/StringExtensions.cs
+++ src/AndcultureCode.CSharp.Extensions/StringExtensions.cs
@@ -60,26 +60,16 @@ namespace AndcultureCode.CSharp.Extensions
                     return match.Groups[1].Value + domainName;
                 }
             }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
             catch (ArgumentException)
             {
                 return false;
             }
 
-            try
-            {
-                return Regex.IsMatch(email,
-                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
-                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-            }
-            catch (RegexMatchTimeoutException)
-            {
-                return false;
-            }
+            return Regex.IsMatch(email,
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+
         }
 
         /// <summary>
