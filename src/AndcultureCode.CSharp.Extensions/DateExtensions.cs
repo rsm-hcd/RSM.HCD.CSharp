044d1d03f6148403bb62d5718817adaa581fa4f0
Jeb Elliott <jelliott@andculture.com>
2019-11-11 15:55:31 -0500

    Update and-cli. Add AtMidnight mehtod and tests

---
+++ src/AndcultureCode.CSharp.Extensions/DateExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndcultureCode.CSharp.Extensions
+{
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Useful when you only care about the date, but do not want to lose the offset.
+        /// Returns the midnight representation of the given DateTimeOffset.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTimeOffset AtMidnight(this DateTimeOffset date)
+        {
+            return new DateTimeOffset(
+                year: date.Year,
+                month: date.Month,
+                day: date.Day,
+                hour: 0,
+                minute: 0,
+                second: 0,
+                offset: date.Offset);
+        }
+    }
+}
---
+++ test/AndcultureCode.CSharp.Extensions.Tests/DateExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shouldly;
+using Xunit;
+
+
+namespace AndcultureCode.CSharp.Extensions.Tests
+{
+    public class DateExtensionsTests
+    {
+        #region AtMidnight
+
+        [Fact]
+        public void AtMidnight_Returns_Date_With_Time_Set_To_Midnight()
+        {
+            // Arrange
+            var testDate = DateTimeOffset.Now;
+
+            // Act
+            var result = testDate.AtMidnight();
+
+            // Assert
+            result.Hour.ShouldBe(0);
+            result.Minute.ShouldBe(0);
+            result.Second.ShouldBe(0);
+
+        }
+
+        [Fact]
+        public void AtMidnight_Preserves_The_Offset_Value()
+        {
+            // Arrange
+            var testDate = DateTimeOffset.Now;
+
+            // Act
+            var result = testDate.AtMidnight();
+
+            // Assert
+            result.Offset.ShouldBe(testDate.Offset);
+        }
+
+        [Fact]
+        public void AtMidnight_Does_Not_Change_The_Date()
+        {
+            // Arrange
+            var testDate = DateTimeOffset.Now;
+
+            // Act
+            var result = testDate.AtMidnight();
+
+            // Assert
+            result.Year.ShouldBe(testDate.Year);
+            result.Month.ShouldBe(testDate.Month);
+            result.Day.ShouldBe(testDate.Day);
+        }
+
+        #endregion
+    }
+}
