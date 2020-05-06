6588dced8de03405186ae6e40a35d2a752eda62e
Brandon Scott <bscott@andculture.com>
2020-05-06 16:34:20 -0400

    Fixed alignment in HttpResponseMessageExtensionsTest

--- test/AndcultureCode.CSharp.Extensions.Tests/HttpResponseMessageExtensionsTest.cs
+++ test/AndcultureCode.CSharp.Extensions.Tests/HttpResponseMessageExtensionsTest.cs
@@ -11,13 +11,13 @@ namespace AndcultureCode.CSharp.Extensions.Tests
         {
             // Arrange
             var response = new HttpResponseMessage();
-            var entity = new UserStub
+            var entity   = new UserStub
             {
-                Name = "andculture engineering",
+                Name         = "andculture engineering",
                 EmailAddress = "developer@andculture.com"
             };
 
-            var jsonEntity = Newtonsoft.Json.JsonConvert.SerializeObject(entity);
+            var jsonEntity   = Newtonsoft.Json.JsonConvert.SerializeObject(entity);
             response.Content = new StringContent(jsonEntity);
 
             // Act
@@ -32,7 +32,7 @@ namespace AndcultureCode.CSharp.Extensions.Tests
         public void FromJson_When_Content_Is_Not_Object_Then_Returns_Parsing_Error()
         {
             // Arrange
-            var response = new HttpResponseMessage();
+            var response     = new HttpResponseMessage();
             response.Content = new StringContent("Hello-World");
 
             // Act & Assert
