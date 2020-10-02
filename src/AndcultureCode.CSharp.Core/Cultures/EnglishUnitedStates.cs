e0c4ac46ed0487b494bf8dbb72e64cd9c5fc6ce9
Craig Banach <craigsb93@gmail.com>
2020-10-02 23:19:05 +0100

    Add XML document comments for Cultures

--- src/AndcultureCode.CSharp.Core/Cultures/EnglishUnitedStates.cs
+++ src/AndcultureCode.CSharp.Core/Cultures/EnglishUnitedStates.cs
@@ -3,9 +3,14 @@ using AndcultureCode.CSharp.Core.Models.Localization;
 
 namespace AndcultureCode.CSharp.Core.Cultures
 {
+    /// <summary>
+    /// Localization Culture for United States English (en-US)
+    /// </summary>
     public class EnglishUnitedStates : Culture
     {
+        /// <inheritdoc/>
         public override string Code { get => Rfc4646LanguageCodes.EN_US; }
+        /// <inheritdoc/>
         public override bool IsDefault => true;
     }
 }
--- src/AndcultureCode.CSharp.Core/Cultures/SpanishSpain.cs
+++ src/AndcultureCode.CSharp.Core/Cultures/SpanishSpain.cs
@@ -3,8 +3,12 @@ using AndcultureCode.CSharp.Core.Models.Localization;
 
 namespace AndcultureCode.CSharp.Core.Cultures
 {
+    /// <summary>
+    /// Localization Culture for Spanish - Spain (es-US)
+    /// </summary>
     public class SpanishSpain : Culture
     {
+        /// <inheritdoc/>
         public override string Code { get => Rfc4646LanguageCodes.ES_ES; }
     }
 }
