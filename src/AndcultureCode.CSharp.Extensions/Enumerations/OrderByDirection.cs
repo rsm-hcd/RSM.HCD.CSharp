74ab824d452cbb64e02291056f3f0c48804f6f30
mkoser-andculture <mkoser@andculture.com>
2020-10-04 11:36:27 -0400

    Added an OrderBy and ThenBy extension method for IOrderedQueryable in the IQueryableExtensions class. Both new functions use a AppendOrderByExpression private method to apply the order by expression.

---
+++ src/AndcultureCode.CSharp.Extensions/Enumerations/OrderByDirection.cs
@@ -0,0 +1,13 @@
+namespace AndcultureCode.CSharp.Extensions.Enumerations
+{
+    /// <summary>
+    /// Directions for ordering items.
+    /// </summary>
+    public enum OrderByDirection
+    {
+        ///<summary>Sorted in ascending order.</summary>
+        Ascending,
+        ///<summary>Sorted in descending order.</summary>
+        Descending,
+    }
+}
--- src/AndcultureCode.CSharp.Extensions/IQueryableExtensions.cs
+++ src/AndcultureCode.CSharp.Extensions/IQueryableExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Linq.Expressions;
+using AndcultureCode.CSharp.Extensions.Enumerations;
 
 namespace AndcultureCode.CSharp.Extensions
 {
@@ -44,6 +45,17 @@ namespace AndcultureCode.CSharp.Extensions
         public static bool IsNullOrEmpty<T>(this IQueryable<T> source, Expression<Func<T, bool>> predicate) => source == null || source.IsEmpty(predicate);
 
         /// <summary>
+        /// Order by the string expression provided.
+        /// </summary>
+        /// <param name="query">The IOrderedQueryable to add additional sorting to.</param>
+        /// <param name="propertyName">The path to the property to order by (e.g. Path.To.Property).</param>
+        /// <param name="direction">Order by which to sort items.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> query,
+            string propertyName, OrderByDirection direction = OrderByDirection.Ascending) => query.AppendOrderByExpression(propertyName, direction, false);
+
+        /// <summary>
         /// Returns a random value in the related IQueryable list
         /// </summary>
         /// <param name="source"></param>
@@ -64,5 +76,56 @@ namespace AndcultureCode.CSharp.Extensions
         /// <param name="source"></param>
         /// <typeparam name="T"></typeparam>
         public static IQueryable<T> Shuffle<T>(this IQueryable<T> source) => source.OrderBy(x => Guid.NewGuid());
+
+        /// <summary>
+        /// Order then by the string expression provided.
+        /// </summary>
+        /// <param name="query">The IOrderedQueryable to add additional sorting to.</param>
+        /// <param name="propertyName">The path to the property to order by (e.g. Path.To.Property).</param>
+        /// <param name="direction">Order by which to sort items.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> query,
+            string propertyName, OrderByDirection direction = OrderByDirection.Ascending) => query.AppendOrderByExpression(propertyName, direction, true);
+
+        #region Private Methods
+
+        /// <summary>
+        /// Adds an additional sorting expression to the supplied IQueryable.
+        /// </summary>
+        /// <param name="query">The IQueryable to add additional sorting to.</param>
+        /// <param name="propertyName">The path to the property to order by (e.g. Path.To.Property).</param>
+        /// <param name="direction">Order by which to sort items.</param>
+        /// <param name="isAdditionalOrderBy">Whether the order by is an addition to an existing OrderBy statement.</param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        private static IOrderedQueryable<T> AppendOrderByExpression<T>(this IQueryable<T> query, string propertyName, OrderByDirection direction, bool isAdditionalOrderBy)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+            {
+                return (IOrderedQueryable<T>)query;
+            }
+
+            ParameterExpression param = Expression.Parameter(typeof(T), string.Empty);
+            Expression body = param;
+
+            foreach (var member in propertyName.Split('.'))
+            {
+                body = Expression.PropertyOrField(body, member);
+            }
+
+            bool isDescending = direction == OrderByDirection.Descending;
+            LambdaExpression sort = Expression.Lambda(body, param);
+            MethodCallExpression call = Expression.Call(
+                typeof(Queryable),
+                (!isAdditionalOrderBy ? "OrderBy" : "ThenBy") + (isDescending ? "Descending" : string.Empty),
+                new[] { typeof(T), body.Type },
+                query.Expression,
+                Expression.Quote(sort));
+
+            return (IOrderedQueryable<T>)query.Provider.CreateQuery<T>(call);
+        }
+
+        #endregion Private Methods
     }
 }
