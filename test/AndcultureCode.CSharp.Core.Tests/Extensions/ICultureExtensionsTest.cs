480aae3487e8b7fd0e9c532a718ef423dcbb1a2b
Said Shah <sshah@andculture.com>
2020-10-05 14:29:16 -0400

    Updated AndcultureCode.CSharp.Testing to 0.4.0 and Added the commented out tests

--- test/AndcultureCode.CSharp.Core.Tests/Extensions/ICultureExtensionsTest.cs
+++ test/AndcultureCode.CSharp.Core.Tests/Extensions/ICultureExtensionsTest.cs
@@ -1,229 +1,228 @@
-/// Temporarily commented up while updating namespace for Result and Error in AndcultureCode.CSharp.Testing
-//using System;
-//using System.Collections.Generic;
-//using Shouldly;
-//using Xunit;
-//using Xunit.Abstractions;
-//using System.Globalization;
-//using AndcultureCode.CSharp.Testing;
-//using AndcultureCode.CSharp.Core.Interfaces;
-//using AndcultureCode.CSharp.Core.Tests.Unit.Stubs;
-//using AndcultureCode.CSharp.Core.Cultures;
-//using AndcultureCode.CSharp.Core.Extensions;
-//using AndcultureCode.CSharp.Testing.Tests;
-//using AndcultureCode.CSharp.Core.Tests.Unit;
+using System;
+using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+using System.Globalization;
+using AndcultureCode.CSharp.Testing;
+using AndcultureCode.CSharp.Core.Interfaces;
+using AndcultureCode.CSharp.Core.Tests.Unit.Stubs;
+using AndcultureCode.CSharp.Core.Cultures;
+using AndcultureCode.CSharp.Core.Extensions;
+using AndcultureCode.CSharp.Testing.Tests;
+using AndcultureCode.CSharp.Core.Tests.Unit;
 
-//namespace AndcultureCode.CSharp.Core.Tests.Extensions
-//{
-//    public partial class ICultureExtensionsTest : CoreUnitTest
-//    {
-//        #region Setup
+namespace AndcultureCode.CSharp.Core.Tests.Extensions
+{
+    public partial class ICultureExtensionsTest : CoreUnitTest
+    {
+        #region Setup
 
-//        public ICultureExtensionsTest(ITestOutputHelper output) : base(output) { }
+        public ICultureExtensionsTest(ITestOutputHelper output) : base(output) { }
 
-//        #endregion Setup
+        #endregion Setup
 
-//        #region Default
+        #region Default
 
-//        [Fact]
-//        public void Default_Given_Cultures_Without_Default_Throws_Exception()
-//        {
-//            Should.Throw<ArgumentException>(() =>
-//            {
-//                new List<ICulture> { new CultureStub() }.Default();
-//            });
-//        }
+        [Fact]
+        public void Default_Given_Cultures_Without_Default_Throws_Exception()
+        {
+            Should.Throw<ArgumentException>(() =>
+            {
+                new List<ICulture> { new CultureStub() }.Default();
+            });
+        }
 
-//        [Fact]
-//        public void Default_Given_Cultures_With_Multiple_Defaults_Throws_Exception()
-//        {
-//            Should.Throw<ArgumentException>(() =>
-//            {
-//                new List<ICulture> { new EnglishUnitedStates(), new EnglishUnitedStates() }.Default();
-//            });
-//        }
+        [Fact]
+        public void Default_Given_Cultures_With_Multiple_Defaults_Throws_Exception()
+        {
+            Should.Throw<ArgumentException>(() =>
+            {
+                new List<ICulture> { new EnglishUnitedStates(), new EnglishUnitedStates() }.Default();
+            });
+        }
 
-//        [Fact]
-//        public void Default_Given_Cultures_With_One_Default_Returns_Default_Culture()
-//        {
-//            // Arrange
-//            var cultures = new List<ICulture>
-//            {
-//                new CultureStub(),
-//                new EnglishUnitedStates() // <----- default
-//            };
+        [Fact]
+        public void Default_Given_Cultures_With_One_Default_Returns_Default_Culture()
+        {
+            // Arrange
+            var cultures = new List<ICulture>
+            {
+                new CultureStub(),
+                new EnglishUnitedStates() // <----- default
+            };
 
-//            // Act
-//            var result = cultures.Default();
+            // Act
+            var result = cultures.Default();
 
-//            // Assert
-//            result.ShouldNotBeNull();
-//            result.IsDefault.ShouldBeTrue();
-//            result.ShouldBeOfType<EnglishUnitedStates>();
-//        }
+            // Assert
+            result.ShouldNotBeNull();
+            result.IsDefault.ShouldBeTrue();
+            result.ShouldBeOfType<EnglishUnitedStates>();
+        }
 
-//        #endregion Default
+        #endregion Default
 
-//        #region Exists
+        #region Exists
 
-//        [Fact]
-//        public void Exists_When_Cultures_IsNull_Returns_False()
-//        {
-//            ICultureExtensions.Exists(cultures: null, cultureCode: "does-not-matter").ShouldBeFalse();
-//        }
+        [Fact]
+        public void Exists_When_Cultures_IsNull_Returns_False()
+        {
+            ICultureExtensions.Exists(cultures: null, cultureCode: "does-not-matter").ShouldBeFalse();
+        }
 
-//        [Fact]
-//        public void Exists_When_Cultures_IsEmpty_Returns_False()
-//        {
-//            new List<ICulture>().Exists(cultureCode: "does-not-matter").ShouldBeFalse();
-//        }
+        [Fact]
+        public void Exists_When_Cultures_IsEmpty_Returns_False()
+        {
+            new List<ICulture>().Exists(cultureCode: "does-not-matter").ShouldBeFalse();
+        }
 
-//        [Fact]
-//        public void Exists_When_CultureCode_DoesNotExist_Returns_False()
-//        {
-//            new List<ICulture> { new CultureStub() }.Exists("404").ShouldBeFalse();
-//        }
+        [Fact]
+        public void Exists_When_CultureCode_DoesNotExist_Returns_False()
+        {
+            new List<ICulture> { new CultureStub() }.Exists("404").ShouldBeFalse();
+        }
 
-//        [Fact]
-//        public void Exists_When_CultureCode_Exists_Returns_True()
-//        {
-//            // Arrange
-//            var expected = new CultureStub();
+        [Fact]
+        public void Exists_When_CultureCode_Exists_Returns_True()
+        {
+            // Arrange
+            var expected = new CultureStub();
 
-//            // Act & Assert
-//            new List<ICulture> { expected }.Exists(expected.Code).ShouldBeTrue();
-//        }
+            // Act & Assert
+            new List<ICulture> { expected }.Exists(expected.Code).ShouldBeTrue();
+        }
 
-//        #endregion Exists
+        #endregion Exists
 
-//        #region ToCultureCodes
+        #region ToCultureCodes
 
-//        [Fact]
-//        public void ToCultureCodes_Without_Arguments_Returns_ListOf_CultureCodes()
-//        {
-//            // Arrange
-//            var expected = new CultureStub();
-//            var expected2 = new EnglishUnitedStates();
-//            var cultures = new List<ICulture> { expected, expected2 };
+        [Fact]
+        public void ToCultureCodes_Without_Arguments_Returns_ListOf_CultureCodes()
+        {
+            // Arrange
+            var expected = new CultureStub();
+            var expected2 = new EnglishUnitedStates();
+            var cultures = new List<ICulture> { expected, expected2 };
 
-//            // Act
-//            var result = cultures.ToCultureCodes();
+            // Act
+            var result = cultures.ToCultureCodes();
 
-//            // Assert
-//            result.ShouldNotBeNull();
-//            result.ShouldContain(expected.Code);
-//            result.ShouldContain(expected2.Code);
-//        }
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldContain(expected.Code);
+            result.ShouldContain(expected2.Code);
+        }
 
-//        [Fact]
-//        public void ToCultureCodes_Given_Delimiter_IsNull_Throws_Exception()
-//        {
-//            Should.Throw<Exception>(() =>
-//            {
-//                new List<ICulture> { new CultureStub() }.ToCultureCodes(null);
-//            });
-//        }
+        [Fact]
+        public void ToCultureCodes_Given_Delimiter_IsNull_Throws_Exception()
+        {
+            Should.Throw<Exception>(() =>
+            {
+                new List<ICulture> { new CultureStub() }.ToCultureCodes(null);
+            });
+        }
 
-//        [Theory]
-//        [InlineData("-")]
-//        [InlineData("- ")]
-//        [InlineData("--")]
-//        [InlineData("- -")]
-//        [InlineData(" -")]
-//        [InlineData(" - ")]
-//        public void ToCultureCodes_Given_Delimiter_IsHyphen_Throws_Exception(string hyphenatedDelimiter)
-//        {
-//            Should.Throw<Exception>(() =>
-//            {
-//                new List<ICulture> { new CultureStub() }.ToCultureCodes(hyphenatedDelimiter);
-//            });
-//        }
+        [Theory]
+        [InlineData("-")]
+        [InlineData("- ")]
+        [InlineData("--")]
+        [InlineData("- -")]
+        [InlineData(" -")]
+        [InlineData(" - ")]
+        public void ToCultureCodes_Given_Delimiter_IsHyphen_Throws_Exception(string hyphenatedDelimiter)
+        {
+            Should.Throw<Exception>(() =>
+            {
+                new List<ICulture> { new CultureStub() }.ToCultureCodes(hyphenatedDelimiter);
+            });
+        }
 
-//        [Fact]
-//        public void ToCultureCodes_Given_Delimiter_IsEmptyString_Throws_Exception()
-//        {
-//            Should.Throw<Exception>(() =>
-//            {
-//                new List<ICulture> { new CultureStub() }.ToCultureCodes("");
-//            });
-//        }
+        [Fact]
+        public void ToCultureCodes_Given_Delimiter_IsEmptyString_Throws_Exception()
+        {
+            Should.Throw<Exception>(() =>
+            {
+                new List<ICulture> { new CultureStub() }.ToCultureCodes("");
+            });
+        }
 
-//        [Theory]
-//        [InlineData(" ")]
-//        [InlineData("|")]
-//        [InlineData(".")]
-//        [InlineData("@")]
-//        public void ToCultureCodes_Given_Delimiter_IsValid_Returns_Custom_Delimited_Codes(string expectedDelimiter)
-//        {
-//            // Arrange
-//            var expectedCode = new CultureStub().Code;
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("|")]
+        [InlineData(".")]
+        [InlineData("@")]
+        public void ToCultureCodes_Given_Delimiter_IsValid_Returns_Custom_Delimited_Codes(string expectedDelimiter)
+        {
+            // Arrange
+            var expectedCode = new CultureStub().Code;
 
-//            // Act
-//            var result = new List<ICulture> { new CultureStub(), new EnglishUnitedStates() }.ToCultureCodes(expectedDelimiter);
+            // Act
+            var result = new List<ICulture> { new CultureStub(), new EnglishUnitedStates() }.ToCultureCodes(expectedDelimiter);
 
-//            // Assert
-//            result.ShouldNotBeNull();
-//            result.ShouldContain(expectedCode);
-//            result.ShouldContain(expectedDelimiter);
-//        }
+            // Assert
+            result.ShouldNotBeNull();
+            result.ShouldContain(expectedCode);
+            result.ShouldContain(expectedDelimiter);
+        }
 
-//        #endregion ToCultureCodes
+        #endregion ToCultureCodes
 
-//        #region ToCultureInfo
+        #region ToCultureInfo
 
-//        [Fact]
-//        public void ToCultureInfo_Given_Null_Throws_Exception()
-//        {
-//            Should.Throw<Exception>(() =>
-//            {
-//                ICultureExtensions.ToCultureInfo(null);
-//            });
-//        }
+        [Fact]
+        public void ToCultureInfo_Given_Null_Throws_Exception()
+        {
+            Should.Throw<Exception>(() =>
+            {
+                ICultureExtensions.ToCultureInfo(null);
+            });
+        }
 
-//        [Fact]
-//        public void ToCultureInfo_Returns_Mapped_CultureInfo()
-//        {
-//            // Arrange
-//            var expected = new CultureStub();
+        [Fact]
+        public void ToCultureInfo_Returns_Mapped_CultureInfo()
+        {
+            // Arrange
+            var expected = new CultureStub();
 
-//            // Act
-//            var result = expected.ToCultureInfo();
+            // Act
+            var result = expected.ToCultureInfo();
 
-//            // Assert
-//            result.ShouldNotBeNull();
-//            result.Name.ShouldBe(expected.Code);
-//        }
+            // Assert
+            result.ShouldNotBeNull();
+            result.Name.ShouldBe(expected.Code);
+        }
 
-//        #endregion ToCultureInfo
+        #endregion ToCultureInfo
 
-//        #region ToCultureInfos
+        #region ToCultureInfos
 
-//        [Fact]
-//        public void ToCultureInfos_Given_List_Containing_Null_Returns_Throws_Exception()
-//        {
-//            Should.Throw<Exception>(() =>
-//            {
-//                new List<ICulture> { new CultureStub(), null }.ToCultureInfos();
-//            });
-//        }
+        [Fact]
+        public void ToCultureInfos_Given_List_Containing_Null_Returns_Throws_Exception()
+        {
+            Should.Throw<Exception>(() =>
+            {
+                new List<ICulture> { new CultureStub(), null }.ToCultureInfos();
+            });
+        }
 
-//        [Fact]
-//        public void ToCultureInfos_Returns_ListOf_Mapped_CultureInfos()
-//        {
-//            // Arrange
-//            var expected = new CultureStub();
-//            var expected2 = new EnglishUnitedStates();
-//            var cultures = new List<ICulture> { expected, expected2 };
+        [Fact]
+        public void ToCultureInfos_Returns_ListOf_Mapped_CultureInfos()
+        {
+            // Arrange
+            var expected = new CultureStub();
+            var expected2 = new EnglishUnitedStates();
+            var cultures = new List<ICulture> { expected, expected2 };
 
-//            // Act
-//            var results = cultures.ToCultureInfos();
+            // Act
+            var results = cultures.ToCultureInfos();
 
-//            // Assert
-//            results.ShouldNotBeEmpty();
-//            results.ShouldContain(e => e.Name == expected.Code);
-//            results.ShouldContain(e => e.Name == expected2.Code);
-//        }
+            // Assert
+            results.ShouldNotBeEmpty();
+            results.ShouldContain(e => e.Name == expected.Code);
+            results.ShouldContain(e => e.Name == expected2.Code);
+        }
 
-//        #endregion ToCultureInfos
-//    }
-//}
+        #endregion ToCultureInfos
+    }
+}
