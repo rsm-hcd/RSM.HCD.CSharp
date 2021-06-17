d5c05e2d01cb6c378708da02d0b97b5062c4996b
Brandon Scott <bscott@andculture.com>
2021-06-17 12:59:36 -0400

    Add inverse IUpdatable matcher extensions + tests

--- src/AndcultureCode.CSharp.Testing/Extensions/IUpdatableMatcherExtensions.cs
+++ src/AndcultureCode.CSharp.Testing/Extensions/IUpdatableMatcherExtensions.cs
@@ -44,5 +44,41 @@ namespace AndcultureCode.CSharp.Testing.Extensions
             entity.ShouldNotBeNull();
             entity.ShouldBeUpdatedBy(updatedBy.Id);
         }
+
+        /// <summary>
+        /// Assert that the `IUpdatable` has a null `UpdatedOn` value
+        /// </summary>
+        /// <param name="entity">Entity under test</param>
+        public static void ShouldNotBeUpdated<TUpdatable>(this TUpdatable entity)
+            where TUpdatable : IUpdatable
+        {
+            entity.ShouldNotBeNull();
+            entity.UpdatedOn.ShouldBeNull();
+        }
+
+        /// <summary>
+        /// Assert that the `IUpdatable` does not match the provided `UpdatedById`
+        /// </summary>
+        /// <param name="entity">Entity under test</param>
+        /// <param name="updatedById">Unexpected id of the record's updater</param>
+        public static void ShouldNotBeUpdatedBy<TUpdatable>(this TUpdatable entity, long updatedById)
+            where TUpdatable : IUpdatable
+        {
+            entity.ShouldNotBeNull();
+            entity.UpdatedById.ShouldNotBe(updatedById);
+        }
+
+        /// <summary>
+        /// Assert that the `IUpdatable` does not match the provided `UpdatedById`
+        /// </summary>
+        /// <param name="entity">Entity under test</param>
+        /// <param name="updatedBy">Unexpected record's updater</param>
+        public static void ShouldNotBeUpdatedBy<TUpdatable>(this TUpdatable entity, IEntity updatedBy)
+            where TUpdatable : IUpdatable
+        {
+            updatedBy.ShouldNotBeNull(ErrorConstants.ERROR_EXPECTED_ENTITY_IS_NULL_MESSAGE);
+            entity.ShouldNotBeNull();
+            entity.ShouldNotBeUpdatedBy(updatedBy.Id);
+        }
     }
 }
--- test/AndcultureCode.CSharp.Testing.Tests/Tests/Unit/Extensions/IUpdatableMatcherExtensionsTest.cs
+++ test/AndcultureCode.CSharp.Testing.Tests/Tests/Unit/Extensions/IUpdatableMatcherExtensionsTest.cs
@@ -160,5 +160,147 @@ namespace AndcultureCode.CSharp.Testing.Tests.Unit.Extensions
         }
 
         #endregion ShouldBeUpdatedBy(IEntity updatedBy)
+
+        #region ShouldNotBeUpdated
+
+        [Fact]
+        public void ShouldNotBeUpdated_When_Entity_Null_Fails_Assertion()
+        {
+            // Arrange
+            IUpdatable entity = null;
+
+            // Act & Assert
+            Should.Throw<ShouldAssertException>(() => entity.ShouldNotBeUpdated());
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdated_When_UpdatedOn_HasValue_Fails_Assertion()
+        {
+            // Arrange
+            var result = Build<UserStub>((e) => e.UpdatedOn = Faker.Date.RecentOffset());
+
+            // Act & Assert
+            Should.Throw<ShouldAssertException>(() => result.ShouldNotBeUpdated());
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdated_When_UpdatedOn_Null_Passes_Assertion()
+        {
+            // Arrange
+            var entity = Build<UserStub>((e) => e.UpdatedOn = null);
+
+            // Act & Assert
+            Should.NotThrow(() => entity.ShouldNotBeUpdated());
+        }
+
+        #endregion ShouldNotBeUpdated
+
+        #region ShouldNotBeUpdatedBy(long updatedById)
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedById_When_Entity_Null_Fails_Assertion()
+        {
+            // Arrange
+            IUpdatable entity = null;
+
+            // Act & Assert
+            Should.Throw<ShouldAssertException>(() => entity.ShouldNotBeUpdatedBy(updatedById: Random.Long()));
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedById_When_UpdatedById_Matches_Fails_Assertion()
+        {
+            // Arrange
+            var expected = Random.Long();
+            var entity = Build<UserStub>((e) => e.UpdatedById = expected);
+
+            // Act & Assert
+            Should.Throw<ShouldAssertException>(() => entity.ShouldNotBeUpdatedBy(updatedById: expected));
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedById_When_UpdatedById_Null_Passes_Assertion()
+        {
+            // Arrange
+            var entity = Build<UserStub>((e) => e.UpdatedById = null);
+
+            // Act & Assert
+            Should.NotThrow(() => entity.ShouldNotBeUpdatedBy(updatedById: Random.Long()));
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedById_When_UpdatedById_Does_Not_Match_Passes_Assertion()
+        {
+            // Arrange
+            var updatedById = Random.Long(min: 1, max: 100);
+            var unexpected = updatedById + 1;
+            var entity = Build<UserStub>((e) => e.UpdatedById = updatedById);
+
+            // Act & Assert
+            Should.NotThrow(() => entity.ShouldNotBeUpdatedBy(updatedById: unexpected));
+        }
+
+        #endregion ShouldNotBeUpdatedBy(long updatedById)
+
+        #region ShouldNotBeUpdatedBy(IEntity updatedBy)
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedBy_When_Expected_Entity_Null_Fails_Assertion()
+        {
+            // Arrange
+            var entity = Build<UserStub>();
+
+            // Act
+            var exception = Record.Exception(() => entity.ShouldNotBeUpdatedBy(updatedBy: (IEntity)null));
+
+            // Assert
+            exception.ShouldNotBeNull();
+            exception.Message.ShouldContain(ErrorConstants.ERROR_EXPECTED_ENTITY_IS_NULL_MESSAGE);
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedBy_When_Entity_Null_Fails_Assertion()
+        {
+            // Arrange
+            IUpdatable entity = null;
+
+            // Act & Assert
+            Should.Throw<ShouldAssertException>(() => entity.ShouldNotBeUpdatedBy(updatedBy: Build<UserStub>()));
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedBy_When_UpdatedById_Matches_Fails_Assertion()
+        {
+            // Arrange
+            var expected = Build<UserStub>((e) => e.UpdatedById = Random.Long());
+            var entity = Build<UserStub>((e) => e.UpdatedById = expected.Id);
+
+            // Act & Assert
+            Should.Throw<ShouldAssertException>(() => entity.ShouldNotBeUpdatedBy(updatedBy: expected));
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedBy_When_UpdatedById_Null_Passes_Assertion()
+        {
+            // Arrange
+            var unexpected = Build<UserStub>((e) => e.UpdatedById = Random.Long());
+            var entity = Build<UserStub>((e) => e.UpdatedById = null);
+
+            // Act & Assert
+            Should.NotThrow(() => entity.ShouldNotBeUpdatedBy(updatedBy: unexpected));
+        }
+
+        [Fact]
+        public void ShouldNotBeUpdatedBy_UpdatedBy_When_UpdatedById_Does_Not_Match_Passes_Assertion()
+        {
+            // Arrange
+            var unexpected = Build<UserStub>((e) => e.UpdatedById = Random.Long(min: 1, max: 100));
+            var entity = Build<UserStub>((e) => e.UpdatedById = unexpected.Id + 1);
+
+            // Act & Assert
+            Should.NotThrow(() => entity.ShouldNotBeUpdatedBy(updatedBy: unexpected));
+        }
+
+        #endregion ShouldNotBeUpdatedBy(IEntity updatedBy)
     }
 }
