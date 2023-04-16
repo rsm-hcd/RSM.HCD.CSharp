b9ae6104d18dbbaa170634da1a471ba3a3249737
Michael Tyson <myty@users.noreply.github.com>
2023-04-16 16:30:38 -0400

    move IsNullOrEmpty and Join to Core project

--- src/Core/Extensions/EnumerableExtensions.cs
+++ src/Core/Extensions/EnumerableExtensions.cs
@@ -24,11 +24,34 @@ namespace AndcultureCode.CSharp.Core.Extensions
             source == null || source.IsEmpty();
 
         /// <summary>
+        /// Determines if the source list is null or empty
+        /// </summary>
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source, Func<T, bool> predicate) =>
+            source == null || source.IsEmpty(predicate);
+
+        /// <summary>
         /// Convenience method so joining strings reads better :)
         /// </summary>
         public static string Join(this IEnumerable<string> list, string delimiter = ", ") =>
             list == null
                 ? null
                 : string.Join(delimiter, list);
+
+        /// <summary>
+        /// Convenience method for joining dictionary key values into a string
+        /// </summary>
+        public static string Join(
+            this IEnumerable<KeyValuePair<string, string>> list,
+            string keyValueDelimiter,
+            string delimiter = ", "
+        ) => list?.Select(e => e.Join(keyValueDelimiter)).Where(e => !string.IsNullOrEmpty(e)).Join(delimiter);
+
+        /// <summary>
+        /// Convenience method for joining key value pairs
+        /// </summary>
+        public static string Join(this KeyValuePair<string, string> pair, string delimiter) =>
+            new List<string> { pair.Key, pair.Value }
+                .Where(e => !string.IsNullOrEmpty(e))
+                .Join(delimiter);
     }
 }
--- src/Extensions/IEnumerableExtensions.cs
+++ src/Extensions/IEnumerableExtensions.cs
@@ -55,41 +55,6 @@ namespace AndcultureCode.CSharp.Extensions
         }
 
         /// <summary>
-        /// Determines if the source list is null or empty
-        /// </summary>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source) =>
-            CoreExtensions.EnumerableExtensions.IsNullOrEmpty(source);
-
-        /// <summary>
-        /// Determines if the source list is null or empty
-        /// </summary>
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> source, Func<T, bool> predicate) =>
-            source == null || source.IsEmpty(predicate);
-
-        /// <summary>
-        /// Convenience method so joining strings reads better :)
-        /// </summary>
-        public static string Join(this IEnumerable<string> list, string delimiter = ", ") =>
-            CoreExtensions.EnumerableExtensions.Join(list, delimiter);
-
-        /// <summary>
-        /// Convenience method for joining dictionary key values into a string
-        /// </summary>
-        public static string Join(
-            this IEnumerable<KeyValuePair<string, string>> list,
-            string keyValueDelimiter,
-            string delimiter = ", "
-        ) => list?.Select(e => e.Join(keyValueDelimiter)).Where(e => !string.IsNullOrEmpty(e)).Join(delimiter);
-
-        /// <summary>
-        /// Convenience method for joining key value pairs
-        /// </summary>
-        public static string Join(this KeyValuePair<string, string> pair, string delimiter) =>
-            new List<string> { pair.Key, pair.Value }
-                .Where(e => !string.IsNullOrEmpty(e))
-                .Join(delimiter);
-
-        /// <summary>
         /// Returns a random value in the related IEnumerable list
         /// </summary>
         public static T PickRandom<T>(this IEnumerable<T> source) => source.PickRandom(1).SingleOrDefault();
