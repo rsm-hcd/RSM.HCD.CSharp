e3062098891d6de7a6aafb3d3fbf90520bcd5037
Michael Tyson <myty@users.noreply.github.com>
2024-05-13 10:49:01 -0400

    Scrub Remaining Andculture references (#60)
    * chore: Update package names and descriptions to reflect RSM.HCD.CSharp rebranding
    
    * chore: Update tagline in docusaurus.config.js file
    
    * chore: Update package names and descriptions to reflect RSM.HCD.CSharp rebranding
    
    * Update README.md
    
    Co-authored-by: Jim Stevenson <jim.stevenson@rsmus.com>
    
    * chore: Update package names and descriptions to reflect RSM.HCD.CSharp rebranding
    
    * chore: Update markdown-toc and markdown.extension.toc settings
    
    Update the markdown-toc.depthFrom setting to 2 and markdown.extension.toc.levels to "2..6" in the .vscode/settings.json file.
    
    ---------
    
    Co-authored-by: Jim Stevenson <jim.stevenson@rsmus.com>

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
     title: "RSM.HCD.CSharp.Core",
     tagline:
-        "Commonly used interfaces, patterns and utilities by andculture engineering",
-    url: "https://andculturecode.github.io",
+        "Commonly used interfaces, patterns and utilities by the HCD engineering team at RSM",
+    url: "https://rsm-hcd.github.io",
     baseUrl: "/RSM.HCD.CSharp.Core/",
     onBrokenLinks: "throw",
     onBrokenMarkdownLinks: "warn",
     favicon: "img/favicon.ico",
-    organizationName: "AndcultureCode",
+    organizationName: "rsm-hcd",
     projectName: "RSM.HCD.CSharp.Core",
     themeConfig: {
         prism: {
@@ -41,7 +41,7 @@ module.exports = {
                     ],
                 },
             ],
-            copyright: `Copyright © ${new Date().getFullYear()} andculture, Inc. Built with Docusaurus.`,
+            copyright: `Copyright © ${new Date().getFullYear()} RSM US, LLP. Built with Docusaurus.`,
         },
     },
     presets: [
